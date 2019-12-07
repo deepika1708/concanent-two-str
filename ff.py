ef3e1edfe24a588e2041617e20db0920ffcffe59
deepika1708 <54228120+deepika1708@users.noreply.github.com>
2019-12-07 22:16:05 +0530

    Create ff.py

---
+++ ff.py
@@ -0,0 +1,7 @@
+
+    print("\t\t\t\tCONCANENT TWO STRINGS\n") 	
+    a = input("Enter first strings : ")  
+    b = input("Enter second strings : ")
+    c = a + b
+    print("Concanent strings is : ",c)   	
+    
